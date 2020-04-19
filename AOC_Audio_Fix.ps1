f9ee9f679dca3a45d0e5aaadb605e53cb6427408
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2020-04-19 19:57:04 +0300

    Update AOC_Audio_Fix.ps1

--- AOC_Audio_Fix.ps1
+++ AOC_Audio_Fix.ps1
@@ -6,7 +6,7 @@
 
 # Turns off the display
 (Add-Type '[DllImport("user32.dll")]public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)
-# Amount of time (in seconds) to wait before the display will be automatically turned ok
+# Amount of time (in seconds) to wait before the display will be automatically turned on
 Start-Sleep -Seconds 1
 # Simulates keyboard space key press to awake the display
 (New-Object -com "Wscript.Shell").SendKeys(" ")
